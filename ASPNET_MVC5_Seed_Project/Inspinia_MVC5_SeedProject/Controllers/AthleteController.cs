a2d9b0cc8a033d3b62b5b031a05b3980c3c21816
HelgiThomas <helgi94@gmail.com>
2017-08-01 08:57:45 +0000

    create,index,edit on athlete

--- ASPNET_MVC5_Seed_Project/Inspinia_MVC5_SeedProject/Controllers/AthleteController.cs
+++ ASPNET_MVC5_Seed_Project/Inspinia_MVC5_SeedProject/Controllers/AthleteController.cs
@@ -1,7 +1,10 @@
 ﻿using Inspinia_MVC5_SeedProject.DAL;
+using Inspinia_MVC5_SeedProject.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -15,10 +18,94 @@ namespace Inspinia_MVC5_SeedProject.Controllers
         // GET: Athlete
         public ActionResult Index()
         {
-            var students = db.Athletes.ToList();
+            var athletes = db.Athletes.ToList();
 
-            return View(students);
+            return View(athletes);
         }
-        
+        // Takes in the id of the athlete that you want to view and does some varification and then reutrns the athlete into the details
+        public ActionResult Details (int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+           // Find the athlete in the database 
+            Athlete athlete = db.Athletes.Find(id);
+
+            if (athlete == null)
+            {
+                return HttpNotFound();
+            }
+            return View(athlete);
+        }
+
+        // GET: Student/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Name, Sex, SocialSecurity, Sport, Team")]Athlete athlete)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Athletes.Add(athlete);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DataException /* dex */)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+            return View(athlete);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Athlete athlete = db.Athletes.Find(id);
+            if (athlete == null)
+            {
+                return HttpNotFound();
+            }
+            return View(athlete);
+        }
+
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditPost(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var athleteToUpdate = db.Athletes.Find(id);
+            if (TryUpdateModel(athleteToUpdate, "",
+               new string[] { "Name", "Sex", "SocialSecurity", "Sport", "Team" }))
+            {
+                try
+                {
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+                catch (DataException /* dex */)
+                {
+                    //Log the error (uncomment dex variable name and add a line here to write a log.
+                    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+                }
+            }
+            return View(athleteToUpdate);
+        }
+
     }
 }
